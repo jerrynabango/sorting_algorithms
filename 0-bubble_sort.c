135ae87aec0cf89172a7106e0c3f92b52a0fb932
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-23 20:32:49 +0300

    Edit 0-bubble_sort.c file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,27 @@
 #include "sort.h"
 
 /**
-  * bubble_sort - function that sorts an array of integers
-  * @array: Parameter
-  * @size: size of array
-  * Return: Nothing
-  */
+	* bubble_sort - function that sorts an array of integers
+	* @array: Parameter
+	* @size: size of array
+	* Return: Nothing
+	*/
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int n;
+	size_t i, j;
+	int n;
 
-    for(i = 0; i < size - 1; i++)
-    {
-        for(j = 0; j < size - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                n = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = n;
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				n = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = n;
+				print_array(array, size);
+			}
+		}
+	}
 }
