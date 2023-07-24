c2931655a86648d219a342045f0d096e83ab399e
jerrynabango <jerrynabango9@gmail.com>
2023-07-24 16:01:39 +0300

    shell_sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -29,21 +29,17 @@ void swap(int *a, int *b)
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t sort_1 = 1, sort_2, sort_3;
+	size_t sort_1, sort_2, sort_3;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (sort_1 < (size / 3))
-	{
+	for (sort_1 = 1; sort_1 < (size / 3);)
 		sort_1 = sort_1 * 3 + 1;
-	}
 
-	while (sort_1 >= 1)
+	for (; sort_1 >= 1; sort_1 /= 3)
 	{
-		sort_2 = sort_1;
-
-		while (sort_2 < size)
+		for (sort_2 = sort_1; sort_2 < size; sort_2++)
 		{
 			sort_3 = sort_2;
 			while (sort_3 >= sort_1 && array[sort_3 - sort_1] > array[sort_3])
@@ -51,11 +47,7 @@ void shell_sort(int *array, size_t size)
 				swap(array + sort_3, array + (sort_3 - sort_1));
 				sort_3 -= sort_1;
 			}
-
-			sort_2++;
 		}
-
-		sort_1 /= 3;
 		print_array(array, size);
 	}
 }
