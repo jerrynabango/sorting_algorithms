c9c7dc53f636100704f62f43e268ea5d81063d68
jerrynabango <jerrynabango9@gmail.com>
2023-07-24 12:59:13 +0300

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,54 +1,37 @@
 #include "sort.h"
 
 /**
- * locate_min - Locatin the min From Current index in the array
- * @array: The Array to be Searched
- * @index: Starting Index of The Search
- * @size: The Size of The Array
- * Return: (int) index of min if found or
- * same given index if index is the min
- */
-
-int locate_min(int *array, int index, size_t size)
-{
-	int min, idx_min;
-	int i;
-
-	min = array[index];
-	idx_min = index;
-	for (i = index; i < (int)size; i++)
-	{
-		if (array[i] < min)
-		{
-			min = array[i];
-			idx_min = i;
-		}
-	}
-	if (idx_min == index)
-		return (-1);
-	return (idx_min);
-}
-
-/**
- * selection_sort - Implementation of selection Sort Algrithme
- * @array: Array to sort type int *
- * @size: The Size of The Given Array
+ * selection_sort - sorts an array of integers in asc using Selection sort
  *
- * Return: (Void) Sorted Array
- */
+ * @array: Indicates pointer to an array of integers
+ *
+ * @size: Indictes the size of the array in ascending order of the elements
+ *
+ * Return: Nothing
+*/
 void selection_sort(int *array, size_t size)
 {
-	int i;
-	int min, tmp;
+	size_t i, j, h;
+	int n;
 
-	for (i = 0; i < (int)size; i++)
+	for (j = 0; j < size - 1; j++)
 	{
-		min = locate_min(array, i, size);
-		if (min != -1)
+		h = j;
+		i = j + 1;
+		while (i < size)
 		{
-			tmp = array[i];
-			array[i] = array[min];
-			array[min] = tmp;
+			if (array[i] < array[h])
+			{
+				h = i;
+			}
+			i++;
+		}
+
+		if (h != j)
+		{
+			n = array[h];
+			array[h] = array[j];
+			array[j] = n;
 			print_array(array, size);
 		}
 	}
