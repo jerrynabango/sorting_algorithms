8c083bdda8758d1c41c9f216061038db85c28a01
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-24 12:49:11 +0300

    Edit  file

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,9 +8,9 @@
  */
 void swap(int *l, int *z)
 {
-    int temp = *l;
-    *l = *z;
-    *z = temp;
+	int temp = *l;
+	*l = *z;
+	*z = temp;
 }
 
 /**
@@ -23,27 +23,27 @@ void swap(int *l, int *z)
  */
 void lomuto_qsort(int *array, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-        int piv = array[high];
-        int l = low - 1, z;
+	if (low < high)
+	{
+		int piv = array[high];
+		int l = low - 1, z;
 
-        for (z = low; z <= high; z++)
-        {
-            if (array[z] <= piv)
-            {
-                l++;
-                if (l != z)
-                {
-                    swap(&array[l], &array[z]);
-                    print_array(array, size);
-                }
-            }
-        }
+		for (z = low; z <= high; z++)
+		{
+			if (array[z] <= piv)
+			{
+				l++;
+				if (l != z)
+				{
+					swap(&array[l], &array[z]);
+					print_array(array, size);
+				}
+			}
+		}
 
-        lomuto_qsort(array, low, l - 1, size);
-        lomuto_qsort(array, l + 1, high, size);
-    }
+		lomuto_qsort(array, low, l - 1, size);
+		lomuto_qsort(array, l + 1, high, size);
+	}
 }
 
 /**
@@ -54,5 +54,5 @@ void lomuto_qsort(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    lomuto_qsort(array, 0, size - 1, size);
+	lomuto_qsort(array, 0, size - 1, size);
 }
