683c6792af174217475b64c67ad62ab351d8a3ed
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-24 10:00:17 +0300

    Edit  file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,13 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int n;
+	int swapped;
 
 	if (size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -23,7 +25,10 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = n;
 				print_array(array, size);
+				swapped = 1;
 			}
 		}
+		if (!swapped)
+			break;
 	}
 }
