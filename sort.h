71d87cc21a76d376484fd631367501b325bd22c5
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-26 00:33:28 +0300

    Edit  file

--- sort.h
+++ sort.h
@@ -24,7 +24,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void heap_sort(int *array, size_t size);
